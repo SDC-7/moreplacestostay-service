8eb362e41d83bbf8a4389b56161a2957e8e64c7b
Josh Rogen <rogenjh@gmail.com>
2020-08-31 15:59:56 -0400

    This commit adds Mongo routes integrated with Express

--- generator.js
+++ generator.js
@@ -3,17 +3,18 @@ const faker = require('faker')
 const argv = require('yargs').argv
 const helpers = require('./helpers/helpers.js')
 
-const residences = argv.residences || 10000000
+const residences = argv.residences || 0
 const filename = argv.output || 'residences.csv'
 const stream = fs.createWriteStream(filename)
 
-const createResidence = () => {
+const createResidence = (i) => {
+  const _id = i;
   const residenceName = faker.company.companyName();
   const price = faker.commerce.price();
   const lat = faker.address.latitude();
   const long = faker.address.longitude();
   const url = helpers.generateRandomImage();
-  return `${residenceName},${price},${lat},${long},${url}\n`
+  return `${_id},"${residenceName}",${price},${lat},${long},${url}\n`
 }
 
 const startWriting = (writeStream, encoding, done) => {
@@ -21,23 +22,23 @@ const startWriting = (writeStream, encoding, done) => {
   function writing() {
     let canWrite = true
     do {
-      i--
-      let residence = createResidence();
-      if (i === 0) {
+      i++
+      let residence = createResidence(i);
+      if (i === 10000000) {
         writeStream.write(residence, encoding, done)
       } else {
         writeStream.write(residence, encoding)
       }
-    } while (i > 0 && canWrite)
+    } while (i < 10000000 && canWrite)
 
-      if (i > 0 && !canWrite) {
+      if (i < 10000000 && !canWrite) {
         writeStream.once('drain', writing);
       }
     }
     writing();
   }
 
-  stream.write(`residenceName,price,lat,long,url\n`,'utf-8')
+  stream.write(`_id,residenceName,price,lat,long,url\n`,'utf-8')
   startWriting(stream, 'utf-8', () => {
     stream.end();
   })
---
+++ server/mongo.js
@@ -0,0 +1,62 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://localhost:27017';
+const collection = 'airbnb_nearby_residences';
+var db;
+
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  db = client.db(collection)
+
+  app.get('/:id', (req, res) => {
+    var id = Number(req.params.id);
+    findLatitudeAndLongitude(db, id, (data) => {
+      if (data === 'Error') return res.sendStatus(500);
+      res.send(data)
+    })
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+});
+
+const findLatitudeAndLongitude = function(db, id, callback) {
+  var collection = db.collection('residences');
+  collection.findOne({'_id': id})
+  .then((data) => {
+    findDocuments(db, data.long, data.lat, callback)
+  })
+  .catch((err) => {
+    callback('Error')
+  })
+}
+
+var findDocuments = function(db, long, lat, callback) {
+  var collection = db.collection('residences');
+  // Find some documents
+  collection.find(
+	{ location :
+	  { $nearSphere :
+	    { $geometry:
+        { type: "Point",
+          coordinates: [ long, lat ] },
+          $minDistance: 1,
+          $maxDistance: 60000
+	    }
+	  }
+	}
+  ).toArray(function(err, docs) {
+    assert.equal(err, null);
+    callback(docs);
+  });
+}
+
+
+
