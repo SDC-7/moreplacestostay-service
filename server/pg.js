0160c8e299e8550117101d9bc40c89bd91b82f0b
Josh Rogen <rogenjh@gmail.com>
2020-09-08 15:12:23 -0400

    adding server route

--- server/pg.js
+++ server/pg.js
@@ -9,7 +9,11 @@ app.use(cors())
 const port = process.env.PORT || 8080;
 require('dotenv').config()
 
+app.get('/loaderio-4569e54d5233e4de49d962f2b850909a/', (req, res) => {
+  res.send('loaderio-4569e54d5233e4de49d962f2b850909a')
+})
 app.use('/:id', express.static('dist'))
+
 
 const mountRoutes = require('../routes')
 mountRoutes(app)
