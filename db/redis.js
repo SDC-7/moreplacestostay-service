bfb566a94e5dbf1b2cdec32b17314196d85e61a4
Josh Rogen <rogenjh@gmail.com>
2020-09-10 16:49:58 -0400

    added config for redis ec2

--- db/redis.js
+++ db/redis.js
@@ -1,5 +1,16 @@
 const redis = require("redis");
-const client = redis.createClient();
+
+const options = {
+  port: '6379',
+  host: '3.86.206.32',
+  password: process.env.REDIS_PASSWORD
+}
+
+const client = redis.createClient(options);
+
+client.on("ready", function() {
+  console.log('redis connected!')
+})
 
 client.on("error", function(error) {
   console.error(error);
